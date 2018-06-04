16ccfa64c705d26d4a97702be844fd0a79616b7d
Unknown <kimsinkyo@naver.com>
2018-06-04 12:30:42 +0900

    자바스크립트 3일차
    수학관련 메소드 예제

---
+++ javascript/day3/JS/math.js
@@ -0,0 +1,20 @@
+//파이
+console.log("Math.PI : " + Math.PI);
+//자연상수 e
+console.log("Math.E : " + Math.E);
+//제곱
+console.log("Math.pow(2,3) : " + Math.pow(2,3));
+//제곱근
+console.log("Math.sqrt(4) : " + Math.sqrt(4));
+//올림
+console.log("Math.ceil(1.3) : " + Math.ceil(1.3));
+//작은 수 반환
+console.log("Math.min(1,4) : " + Math.min(1,4));
+//큰 수 반환
+console.log("Math.max(1,4) : " + Math.max(1,4));
+//랜덤 한 수
+console.log("Math.random() : " + Math.random());
+//반올림
+console.log("Math.round(1.3) : " + Math.round(1.3));
+//내림
+console.log("Math.floor(1.9) : " + Math.floor(1.9));
