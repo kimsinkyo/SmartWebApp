1aefe720d59a2de9fcc39b9a26add9722692117f
Unknown <kimsinkyo@naver.com>
2018-07-09 09:10:49 +0900

    포폴 서버연동
    포트폴리오 프론트 페이지를 서버와 연동하기

--- portfolio/front-end/js/page_1.js
+++
@@ -1,8 +0,0 @@
-$("#hamburger").click(function() {
-  $("#sidenav").css("right", "0");
-  $("body").addClass("dark")
-});
-$("#close-sidenav").click(function() {
-	$("#sidenav").css("right", "-150px");
-  $("body").removeClass("dark");
-});
