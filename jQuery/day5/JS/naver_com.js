2273bd126d31db5cb0f14affa3904b8d180ac136
Unknown <kimsinkyo@naver.com>
2018-07-24 10:14:35 +0900

    네이버 페이지 만들기 3회차
    쥬니어네이버, 해피빈 데코

--- jQuery/day5/JS/naver_com.js
+++ jQuery/day5/JS/naver_com.js
@@ -8,6 +8,7 @@ $("td").mouseleave(function(){
   $("div.is_hovered").remove();
 });
 
+// 실시간 검색어 J쿼리문
 function list(){
   $("#news01 li:first").slideUp(function(){
     $(this).appendTo($("#news01")).slideDown();
@@ -15,9 +16,38 @@ function list(){
 }
 setInterval(function(){list()},4000);
 
+// 실시간 검색어 팝업
+$(document).ready(function(){
+$("#RTWord").hover(function(){
+  $(".search-hover").toggleClass('display-block');
+});
+  $("#auto-icon").click(function(){
+    $(".sub-menu").toggleClass('display-block');
+  });
+});
+
+// 쥬니어네이버 색 변화
+$(document).ready(function(){
+  $(".junior_naver").hover(function(){
+    $("#blue_ju").toggleClass('blue');
+    $("#orange_ni").toggleClass('orange');
+    $("#red_uh").toggleClass('red');
+    $("#naver_green").toggleClass('green');
+  });
+});
+
+// 해피빈 색 변화
+$(document).ready(function(){
+  $(".happy_bean").hover(function(){
+    $("#happy").toggleClass('happy');
+    $("#bean").toggleClass('bean');
+  });
+});
+// 뉴스 J쿼리문
 function RTlist(){
   $("#RTWord li:first").slideUp(function(){
     $(this).appendTo($("#RTWord")).slideDown();
   });
 }
 setInterval(function(){RTlist()},4000);
+
